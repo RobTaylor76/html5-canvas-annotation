37a3ba662180f52b99a69da8f9742d8ce6d9a7a2
Rob Taylor <robert.taylor@fullstackconsultancy.com>
2017-06-19 17:04:50 +0100

    annotation and combination of line

--- js/lineDrawing.js
+++ js/lineDrawing.js
@@ -1,3 +1,136 @@
-/**
- * Created by rob on 19/06/17.
- */
+/* http://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
+
+<canvas id="sig-canvas" width="320" height="160">
+    Get a better browser, bro.
+</canvas>
+*/
+
+
+var LineDrawing = function(config) {
+    this.setConfig(config);
+};
+
+
+LineDrawing.prototype.setConfig = function(customConfig) {
+
+    this.config = customConfig;
+
+    if (!this.config.strokeStyle) {
+        this.config.strokeStyle = "#222222";
+    }
+
+    if ( (!this.config.lineWidth) || (this.config.lineWidth<1) ) {
+        this.config.lineWidth = 2;
+    }
+};
+
+
+LineDrawing.prototype.attachToCanvas = function(canvas) {
+    this.canvas = canvas;
+    this.canvasContext = canvas.getContext("2d");
+    this.canvasContext.strokeStyle = this.config.strokeStyle;
+    this.canvasContext.lineWidth = this.config.lineWidth;
+
+    var This = this;
+
+    canvas.addEventListener("mousedown", function (e) {
+        e.preventDefault();
+        This.drawing = true;
+        This.lastPos = This.getMousePos(e);
+        This.mousePos = This.lastPos;
+    }, false);
+
+    canvas.addEventListener("mouseup", function (e) {
+        e.preventDefault();
+        This.drawing = false;
+    }, false);
+
+    canvas.addEventListener("mousemove", function (e) {
+        e.preventDefault();
+        This.mousePos = This.getMousePos(e);
+    }, false);
+
+    // Set up touch events for mobile, etc
+    canvas.addEventListener("touchstart", function (e) {
+        e.preventDefault();
+        var touchPos = This.getTouchPos(e);
+        var mouseEvent = new MouseEvent("mousedown", {
+            clientX: touchPos.x,
+            clientY: touchPos.y
+        });
+        This.canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    canvas.addEventListener("touchend", function (e) {
+        e.preventDefault();
+        var mouseEvent = new MouseEvent("mouseup", {});
+        This.canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+    canvas.addEventListener("touchmove", function (e) {
+        e.preventDefault();
+        var touchPos = This.getTouchPos(e);
+        var mouseEvent = new MouseEvent("mousemove", {
+            clientX: touchPos.x,
+            clientY: touchPos.y
+        });
+        This.canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+   var animateCallback  = function() {
+       requestAnimationFrame(animateCallback);
+       This.renderCanvas();
+   };
+   requestAnimationFrame(animateCallback);
+};
+
+// Get the position of the mouse relative to the canvas
+LineDrawing.prototype.getMousePos = function(mouseEvent) {
+    var rect = this.canvas.getBoundingClientRect();
+ //   var rect = this.getCanvasPosition();
+    return {
+        x: mouseEvent.clientX - rect.left,
+        y: mouseEvent.clientY - rect.top
+    };
+};
+
+// Draw to the canvas
+LineDrawing.prototype.renderCanvas = function() {
+    if (this.drawing) {
+        this.canvasContext.moveTo(this.lastPos.x, this.lastPos.y);
+        this.canvasContext.lineTo(this.mousePos.x, this.mousePos.y);
+        this.canvasContext.stroke();
+        this.lastPos = this.mousePos;
+    }
+};
+
+// Get the position of a touch relative to the canvas
+LineDrawing.prototype.getTouchPos = function(touchEvent) {
+//    var rect = this.canvas.getBoundingClientRect();
+
+   // var rect = this.getCanvasPosition();
+    return {
+        x: touchEvent.touches[0].clientX, //- rect.left,
+        y: touchEvent.touches[0].clientY //- rect.top
+    };
+};
+
+
+
+LineDrawing.prototype.getCanvasPosition = function() {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    var element = this.canvas;
+
+    while (element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    return {
+        x: xPosition,
+        y: yPosition
+    };
+};
+
